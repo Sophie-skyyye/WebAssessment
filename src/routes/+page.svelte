5e24cd1c6d9b5b5b3aa1508c8dea2513b09028e2
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2023-09-06 08:31:41 +1200

    restarted assessment

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>   
+    import Nav from '$lib/Nav.svelte';
+    import Footer from '$lib/Footer.svelte';
+    import CenterInfo from '$lib/CenterInfo.svelte';
+</script>
+
+<Nav/>
+
+<CenterInfo/>
+
+<Footer/>
