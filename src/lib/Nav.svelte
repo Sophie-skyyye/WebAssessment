46915cc88adfb59105fff6b4e28b48b3e307f0b0
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2023-09-06 09:52:02 +1200

    Logos, text and asthetics added

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,49 @@
+<div class="main">
+    <nav>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img src="images/Hondalogo.PNG" class="logo" >
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img src="images/DelSolbadge.PNG" class="badge" >
+        <ul>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <li><a herf="#">About</a></li>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <li><a herf="#">Specs</a></li>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <li><a herf="#">Parts</a></li>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <li><a herf="#">Builds</a></li>
+        </ul>                            
+    </nav>   
+</div>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<img src="images/DelSolOpening.png" class="openImage" >
+
+<h2 class = "title">Your dream Honda is waiting…</h2>
+<div class = "dreamText">
+    <p>
+        The Honda C-RX one of the worlds most under rated honda's. 
+        Yes it leaks and squeaks when it rains and most of the parts are discontinued
+    </p>
+</div>
+
+<div class = "about">
+    <h4> About</h4>
+    The Honda CR-X del Sol is a two-seater targa-top 
+    car manufactured by Honda from 1992 until 1998. 
+    Despite the body resemblance to a mid-engine car 
+    design, the del Sol is based on the front-engined 
+    Honda Civic platform and was the successor to the Honda CR-X.
+    The Spanish name del Sol translates to of the sun, and 
+    refers to the car's opening roof. The del Sol featured 
+    a removable aluminum hardtop that stowed onto a hinged 
+    frame in the trunk and a motorized drop-down rear window. 
+    Manual and automatic "TransTop" roofs were available 
+    in select markets. It is the first open-air Honda sold in 
+    the United States.
+
+Production and sales ended with the 1997 model in North America and 1998 elsewhere.
+</div>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script>   
     import Nav from '$lib/Nav.svelte';
     import Footer from '$lib/Footer.svelte';
-    import CenterInfo from '$lib/CenterInfo.svelte';
 </script>
 
-<Nav/>
 
-<CenterInfo/>
+
+<Nav/>
 
 <Footer/>
