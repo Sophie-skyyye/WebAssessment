90e1e2b8089eb39a9032417cf9e0b947b262c7cb
Sophie-skyyye <sophieskyed@gmail.com>
2023-09-13 22:13:10 +1200

    removed about section on web page and for CSS

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -20,34 +20,4 @@
 <!-- svelte-ignore a11y-missing-attribute -->
 <img src="images/DelSolOpening.png" class="openImage" >
 
-<h2 class = "title">Your dream Honda is waiting…</h2>
-<div class = "dreamText">
-    <p>The Honda C-RX one of the worlds most under rated honda's of all time.</p>
-    <p>Yes it leaks and squeaks when it rains and most of the parts are discontinued </p>
-    <p>but, that wont stop you from having the best summer of your life. The Del sol </p>
-    <p>fetures a removable hard targa-top for the sunny days, personal safes behind the </p>
-    <p>seats to store any of your personal items while you are taking a dip at the beach. </p>
-    <p>The Del sol isn't just the best car for your adventers but anamzing car for the track</p>
-</div>
-<!-- svelte-ignore a11y-missing-attribute -->
-<img src="images/noRoofSo.PNG" class="sidePov" >
-
-<div class = "about">
-    <h4> About</h4>
-    The Honda CR-X del Sol is a two-seater targa-top 
-    car manufactured by Honda from 1992 until 1998. 
-    Despite the body resemblance to a mid-engine car 
-    design, the del Sol is based on the front-engined 
-    Honda Civic platform and was the successor to the Honda CR-X.
-    The Spanish name del Sol translates to of the sun, and 
-    refers to the car's opening roof. The del Sol featured 
-    a removable aluminum hardtop that stowed onto a hinged 
-    frame in the trunk and a motorized drop-down rear window. 
-    Manual and automatic "TransTop" roofs were available 
-    in select markets. It is the first open-air Honda sold in 
-    the United States.
-
-Production and sales ended with the 1997 model in North America and 1998 elsewhere.
-</div>
-
 
