297aa9cc1eb274e78a77c4e02191fb15265bf1b1
Sophie-skyyye <sophieskyed@gmail.com>
2023-10-25 14:05:07 +1300

    Added info about the car to the page

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -11,5 +11,24 @@
             <li><a href="#">VTI</a></li>
         </ul>
         <a href="#" class="btn">Service Manual</a>
-    </nav>    
+    </nav>
+    <div class="info">
+        <div>
+            <h2>?</h2>
+            <p>0-60 km</p>
+        </div>
+        <div>
+            <h2>180 kmph</h2>
+            <p>Top Speed</p>
+        </div>
+        <div>
+            <h2>?</h2>
+            <p>Max Range</p>
+        </div>
+        <div class="line">
+        </div>
+        <div>
+            <h2>vxi</h2>
+        </div>
+    </div>    
 </div>
