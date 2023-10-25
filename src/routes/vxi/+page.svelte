e33ab79f38bd80267e9cac8b4fc5ba6c2ebe3c8a
Sophie-skyyye <sophieskyed@gmail.com>
2023-10-25 14:20:06 +1300

    added sir page

--- src/routes/vxi/+page.svelte
+++ src/routes/vxi/+page.svelte
@@ -1,1 +1,34 @@
-vxi page
+<div class="header">
+    <nav>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img src="images/logo.PNG" class="logo">
+        <ul>
+            <li><a href="/vxi">VXI</a></li>
+            <li><a href="/sir">SIR</a></li>
+            <li><a href="#">S</a></li>
+            <li><a href="#">SI</a></li>
+            <li><a href="#">ESI</a></li>
+            <li><a href="#">VTI</a></li>
+        </ul>
+        <a href="https://www.crxdelsol.fr/Faq/WorkShop/the_del_Sol_guide_full.pdf" class="btn">Service Manual</a>
+    </nav>
+    <div class="info">
+        <div>
+            <h2>?</h2>
+            <p>0-60 km</p>
+        </div>
+        <div>
+            <h2>180 kmph</h2>
+            <p>Top Speed</p>
+        </div>
+        <div>
+            <h2>?</h2>
+            <p>Max Range</p>
+        </div>
+        <div class="line">
+        </div>
+        <div>
+            <h2>vxi</h2>
+        </div>
+    </div>    
+</div>
