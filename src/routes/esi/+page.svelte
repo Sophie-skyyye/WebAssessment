34fdd19776c709217d42d30d967b7a975cc6a170
Sophie-skyyye <sophieskyed@gmail.com>
2023-10-25 14:23:23 +1300

    Created esi page

---
+++ src/routes/esi/+page.svelte
@@ -0,0 +1,34 @@
+<div class="header">
+    <nav>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img src="images/logo.PNG" class="logo">
+        <ul>
+            <li><a href="/vxi">VXI</a></li>
+            <li><a href="/sir">SIR</a></li>
+            <li><a href="/s">S</a></li>
+            <li><a href="/si">SI</a></li>
+            <li><a href="/esi">ESI</a></li>
+            <li><a href="vti">VTI</a></li>
+        </ul>
+        <a href="https://www.crxdelsol.fr/Faq/WorkShop/the_del_Sol_guide_full.pdf" class="btn">Service Manual</a>
+    </nav>
+    <div class="info">
+        <div>
+            <h2>?</h2>
+            <p>0-60 km</p>
+        </div>
+        <div>
+            <h2>180 kmph</h2>
+            <p>Top Speed</p>
+        </div>
+        <div>
+            <h2>?</h2>
+            <p>Max Range</p>
+        </div>
+        <div class="line">
+        </div>
+        <div>
+            <h2>esi</h2>
+        </div>
+    </div>    
+</div>
