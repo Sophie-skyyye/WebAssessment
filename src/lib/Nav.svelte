28eee2907069b6862750a00fd3d8abd95ef5dc1b
Sophie-skyyye <sophieskyed@gmail.com>
2023-10-25 14:16:45 +1300

    added css for the info section

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -3,14 +3,14 @@
         <!-- svelte-ignore a11y-missing-attribute -->
         <img src="images/logo.PNG" class="logo">
         <ul>
-            <li><a href="#">VXI</a></li>
+            <li><a href="/vxi">VXI</a></li>
             <li><a href="#">SIR</a></li>
             <li><a href="#">S</a></li>
             <li><a href="#">SI</a></li>
             <li><a href="#">ESI</a></li>
             <li><a href="#">VTI</a></li>
         </ul>
-        <a href="#" class="btn">Service Manual</a>
+        <a href="https://www.crxdelsol.fr/Faq/WorkShop/the_del_Sol_guide_full.pdf" class="btn">Service Manual</a>
     </nav>
     <div class="info">
         <div>
