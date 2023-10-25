41556c0de04a6524345fbd9be6ec86ccc5f89efd
Sophie-skyyye <sophieskyed@gmail.com>
2023-10-25 13:36:56 +1300

    Added page links to make the header

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -2,5 +2,13 @@
     <nav>
         <!-- svelte-ignore a11y-missing-attribute -->
         <img src="images/logo.PNG" class="logo">
+        <ul>
+            <li><a href="#">VXI</a></li>
+            <li><a href="#">SIR</a></li>
+            <li><a href="#">S</a></li>
+            <li><a href="#">SI</a></li>
+            <li><a href="#">ESI</a></li>
+            <li><a href="#">VTI</a></li>
+        </ul>
     </nav>    
 </div>
