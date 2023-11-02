037747efffb40c20bbee5cae07821cc99e89fb72
Sophie-skyyye <sophieskyed@gmail.com>
2023-11-02 14:01:02 +1300

    got the background image change to work just had to remove the body css but now wont load up a background image until a link has been clicked

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,30 @@
+<script>
+    let backgroundImage = "../images/image_2.jpg";
+
+    function changeBackgroundImage(image) {
+        backgroundImage = `url('${image}')`;
+    }
+</script>
+
 <svelte:head>
     <link rel="stylesheet" href="css/main.css" />
 </svelte:head>
-<nav>
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <img src="images/logo.PNG" class="logo">
-    <ul>
-        <li on:click={changeBackground()}><a href="/vxi" id="vxi">VXI</a></li>
-        <li><a href="/sir" id="sir">SIR</a></li>
-        <li><a href="/s" id="s">S</a></li>
-        <li><a href="/si" id="si">SI</a></li>
-        <li><a href="/esi" id="esi">ESI</a></li>
-        <li><a href="/vti" id="esi">VTI</a></li>
-    </ul>
-    <a href="https://www.crxdelsol.fr/Faq/WorkShop/the_del_Sol_guide_full.pdf" class="btn">Service Manual</a>
-</nav>
+<div class="container" style="background-image: {backgroundImage}">
+    <nav>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img src="images/logo.PNG" class="logo" />
+        <ul>
+            <li on:click={() => changeBackgroundImage("../images/DelSolOpening.png")}><a href="/vxi" id="vxi">VXI</a></li>
+            <li><a href="/sir" id="sir">SIR</a></li>
+            <li><a href="/s" id="s">S</a></li>
+            <li><a href="/si" id="si">SI</a></li>
+            <li><a href="/esi" id="esi">ESI</a></li>
+            <li><a href="/vti" id="esi">VTI</a></li>
+        </ul>
+        <a
+            href="https://www.crxdelsol.fr/Faq/WorkShop/the_del_Sol_guide_full.pdf"
+            class="btn">Service Manual</a
+        >
+    </nav>
+</div>
 <slot />
-
