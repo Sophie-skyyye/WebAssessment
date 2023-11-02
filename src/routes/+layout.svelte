ee2704367578b6981bca688d32fdde012f50e8bb
Sophie-skyyye <sophieskyed@gmail.com>
2023-11-02 14:52:59 +1300

    new images added and the background image change works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,16 +15,15 @@
         <img src="images/logo.PNG" class="logo" />
         <ul>
             <li on:click={() => changeBackgroundImage("../images/DelSolOpening.png")}><a href="/vxi" id="vxi">VXI</a></li>
-            <li><a href="/sir" id="sir">SIR</a></li>
-            <li><a href="/s" id="s">S</a></li>
-            <li><a href="/si" id="si">SI</a></li>
-            <li><a href="/esi" id="esi">ESI</a></li>
-            <li><a href="/vti" id="esi">VTI</a></li>
+            <li on:click={() => changeBackgroundImage("../images/image_7.jpg")}><a href="/sir" id="sir">SIR</a></li>
+            <li on:click={() => changeBackgroundImage("../images/image_3.png")}><a href="/s" id="s">S</a></li>
+            <li on:click={() => changeBackgroundImage("../images/image_5.png")}><a href="/si" id="si">SI</a></li>
+            <li on:click={() => changeBackgroundImage("../images/image_4.2.jpg")}><a href="/esi" id="esi">ESI</a></li>
+            <li on:click={() => changeBackgroundImage("../images/image_8.jpg")}><a href="/vti" id="esi">VTI</a></li>
         </ul>
         <a
             href="https://www.crxdelsol.fr/Faq/WorkShop/the_del_Sol_guide_full.pdf"
-            class="btn">Service Manual</a
-        >
+            class="btn">Service Manual</a>
     </nav>
 </div>
 <slot />
