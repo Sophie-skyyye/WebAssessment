c9d84e764c2fc1b6a32939dbadfec7a4325daeee
Sophie-skyyye <sophieskyed@gmail.com>
2023-11-02 13:26:21 +1300

    moved nav bar into layout to reduce duplicate code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,18 @@
-<script>
-    var header = document.querySelector("body");
-    var vXi = document.getElementById("vxi");
-    var sIr = document.getElementById("sir");
-    var sI = document.getElementById("si");
-    var S = document.getElementById("s");
-    var eSi = document.getElementById("esi");
-    var vTi = document.getElementById("vti");
-
-    vXi.onclick = function(){
-        body.style.backgroundImage = "url(../images/DelSolOpening.png)";
-    }
-    sIr.onclick = function(){
-        body.style.backgroundImage = "url(../images/image_2.jpeg)";
-    }
-    sI.onclick = function(){
-        body.style.backgroundImage = "url(../images/image_3.png)";
-    }
-    S.onclick = function(){
-        body.style.backgroundImage = "url(../images/image_4.1.jpeg)";
-    }
-    eSi.onclick = function(){
-        body.style.backgroundImage = "url(../images/image_4.2.jpeg)";
-    }
-    vTi.onclick = function(){
-        body.style.backgroundImage = "url(../images/DelSolOpening.png)";
-    }
-</script>
-
-
 <svelte:head>
     <link rel="stylesheet" href="css/main.css" />
 </svelte:head>
-
+<nav>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img src="images/logo.PNG" class="logo">
+    <ul>
+        <li on:click={changeBackground()}><a href="/vxi" id="vxi">VXI</a></li>
+        <li><a href="/sir" id="sir">SIR</a></li>
+        <li><a href="/s" id="s">S</a></li>
+        <li><a href="/si" id="si">SI</a></li>
+        <li><a href="/esi" id="esi">ESI</a></li>
+        <li><a href="/vti" id="esi">VTI</a></li>
+    </ul>
+    <a href="https://www.crxdelsol.fr/Faq/WorkShop/the_del_Sol_guide_full.pdf" class="btn">Service Manual</a>
+</nav>
 <slot />
+
--- src/routes/vti/+page.svelte
+++ src/routes/vti/+page.svelte
@@ -1,20 +1,5 @@
 <div class="body" id="header">
-  <nav>
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <img src="images/logo.PNG" class="logo" />
-    <ul>
-      <li><a href="/vxi" id="vxi">VXI</a></li>
-      <li><a href="/sir" id="sir">SIR</a></li>
-      <li><a href="/s" id="s">S</a></li>
-      <li><a href="/si" id="si">SI</a></li>
-      <li><a href="/esi" id="esi">ESI</a></li>
-      <li><a href="/vti" id="esi">VTI</a></li>
-    </ul>
-    <a
-      href="https://www.crxdelsol.fr/Faq/WorkShop/the_del_Sol_guide_full.pdf"
-      class="btn">Service Manual</a
-    >
-  </nav>
+  
   <div class="info">
     <div>
       <h2>?</h2>
