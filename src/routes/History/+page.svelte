ba9fcc6c7d81da8dc86b16843e0064e2323cb482
Sophie-skyyye <sophieskyed@gmail.com>
2023-11-07 20:15:18 +1300

    added history for CRX

--- src/routes/History/+page.svelte
+++ src/routes/History/+page.svelte
@@ -20,7 +20,17 @@
     <img src={img.urls.regular} class ="CRX" />
 {/each}
 
-
+<div class="crxinfo">
+    <p>The Honda CRX, initially launched in 1983, marked the Japanese automaker's foray 
+        into the burgeoning market for sport compact cars. This two-seater front-wheel-drive 
+        hatchback quickly gained popularity for its agile handling and fuel efficiency, becoming a 
+        symbol of the era's burgeoning interest in compact, performance-oriented vehicles. Its first 
+        generation, produced until 1987, boasted a lightweight design and a peppy engine, appealing to 
+        enthusiasts seeking a blend of nimble handling and practicality. The second generation, introduced 
+        in 1988, underwent significant design changes, featuring a more streamlined appearance and enhanced 
+        performance capabilities. Its discontinuation in 1991 marked the end of an influential chapter in Honda's history, 
+        leaving a lasting legacy as an icon of the brand's commitment to engineering excellence and dynamic driving experiences.</p>
+</div>
 
 
 
